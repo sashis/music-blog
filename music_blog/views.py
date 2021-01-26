3c55afda9ed27a70e6a6627940c984f566ff6b4c
Александр Кропанев <kropanev@mail.ru>
2021-01-26 19:06:03 +0300

    add posts by tag & post detail view

--- music_blog/views.py
+++ music_blog/views.py
@@ -4,28 +4,40 @@ from . import app
 from .models import User, Post, Tag
 
 @app.route('/')
-def posts_list():
+def posts_list(item_id=None):
     page = request.args.get('page', 1, type=int)
-    app.logger.debug(f'Page number: {page}')
-    pagination = Post.query.order_by(Post.created_at.desc()).paginate(
-        page, app.config['POST_PER_PAGE'], False)
-    tags = Tag.query.all()
-    return render_template('posts_all.html', pagination=pagination, tags=tags)
+    posts = Post.query.order_by(Post.created_at.desc())
+    context = {
+        'header': 'Блог о любимой музыке и темах, с ней связанных',
+        'item_id': item_id,
+        'pagination': posts.paginate(page, app.config['POST_PER_PAGE'], False),
+        'tags': Tag.query.all()
+    }
+    return render_template('paginated_list.html', **context)
 
 
 @app.route('/posts/<int:post_id>/')
 def post_detail(post_id):
     post = Post.query.get_or_404(post_id)
-    return f'{post}'
+    return render_template('post_detail.html', post=post)
 
 
 @app.route('/post/<int:post_id>/img/')
 def post_image(post_id):
+    app.logger.debug(f'{request.referrer=}')
     post = Post.query.get_or_404(post_id)
-    app.logger.debug(post.img_filename)
     return send_from_directory(app.config['UPLOADS'], post.img_filename)
 
 
-@app.route('/tag/<int:tag_id>')
-def posts_list_by_tag(tag_id):
-    return f'All posts with tag {tag_id}'
+@app.route('/tag/<int:item_id>/')
+def posts_list_by_tag(item_id):
+    page = request.args.get('page', 1, type=int)
+    tag = Tag.query.get_or_404(item_id)
+    posts_by_tag = tag.posts.order_by(Post.created_at.desc())
+    context = {
+        'header': f'Статьи с тегом "{tag.name}"',
+        'item_id': item_id,
+        'pagination': posts_by_tag.paginate(page, app.config['POST_PER_PAGE'], False),
+        'tags': Tag.query.all()
+    }
+    return render_template('paginated_list.html', **context)
