d5f778418b19e9e55189879306825144f7a064d2
Александр Кропанев <kropanev@mail.ru>
2021-01-27 16:01:18 +0300

    add authentication, auth.login and auth.logout views

--- music_blog/auth.py
+++ music_blog/auth.py
@@ -1,10 +1,12 @@
-from flask import Blueprint
-from flask_login import LoginManager
+from flask import Blueprint, flash, request, render_template, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user
 
 from .models import User
+from .forms import LoginForm
 
-login_manager = LoginManager()
 auth = Blueprint('auth', __name__)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 
 @login_manager.user_loader
@@ -14,7 +16,16 @@ def load_user(user_id):
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            next_url = request.args.get('next')
+            return redirect(next_url or url_for('blog.posts_list'))
+        else:
+            flash("Неверные логин и/или пароль.")
+    return render_template('login.html', form=form)
 
 
 @auth.route('/register', methods=['GET', 'POST'])
@@ -24,4 +35,5 @@ def register():
 
 @auth.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('blog.posts_list'))
---
+++ music_blog/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
